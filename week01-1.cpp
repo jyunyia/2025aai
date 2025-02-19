2060249db7e46c418e32865b36907346fdcd696b
jyunyia <jyunyia@gmail.com>
2025-02-19 11:55:32 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,13 @@
+///week01-1.cpp 兩數相加
+///file-new empty 開新的檔案
+///Ctrl-s 存檔成 week01-1.cpp 要記得加.cpp
+#include <stdio.h>///不能打錯
+///stdio 意思是標準的standard input/output
+///有 stdio.h的外掛才能用scanf() printf*()
+int main()
+{
+	int a,b;///整數a,b
+	scanf("%d",&a);///掃描讀東西
+	scanf("%d",&b);
+	printf("%d",a+b);///印東西
+}
