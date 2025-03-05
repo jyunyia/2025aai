fea58f5ccb16c1805c7a9ce4f0655ed9aec26152
jyunyia <jyunyia@gmail.com>
2025-03-05 11:51:00 +0800

    week03

---
+++ week03-3.cpp
@@ -0,0 +1,16 @@
+///week03-3.cpp
+#include <stdio.h>
+int main()
+{///從零開始,中間寫多少就跑幾次
+    for(int i=0;i<15;i++){
+        printf("%d",i);
+    }
+    printf("\n");
+    ///從1開始,中間寫多少就跑幾次
+    for(int i=1;i<=5;i++){
+        printf("%d",i);
+        }
+
+
+}
+
